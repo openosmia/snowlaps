755ad87a0ee44285db5cd23e6f1b4a875dc00d54
AdrienWehrle <adrien.wehrle@hotmail.fr>
2024-09-29 16:45:46 +0200

    fix start app

--- start_app.sh
+++ start_app.sh
@@ -2,8 +2,5 @@
 
 echo "---------------------"
 echo "Go to http://localhost:8501"
-<<<<<<< HEAD
-python -m streamlit run app/Hello.py
-=======
+
 python -m streamlit run ./app/app.py
->>>>>>> 6922bda96f83a48acaf7776c537789e64bfdb4ff
